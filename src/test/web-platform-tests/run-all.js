ed767ec4befaf3176d71a67918e9df0ecf6177e9
Eyas Sharaiha <eyas.sharaiha@gmail.com>
2022-12-04 20:53:42 -0500

    Enable 8 more tests.

--- src/test/web-platform-tests/run-all.js
+++ src/test/web-platform-tests/run-all.js
@@ -6,7 +6,7 @@ const __dirname = "src/test/web-platform-tests";
 const testFolder = path.join(__dirname, "converted");
 
 let passed = 0;
-let failed = 0;
+const failed = [];
 let skipped = 0;
 
 const skip = [
@@ -42,19 +42,10 @@ const skip = [
     "idbcursor-continue-exception-order.js",
     "idbcursor-delete-exception-order.js",
     "idbcursor-update-exception-order.js",
-    "idbobjectstore-add-put-exception-order.js",
-    "idbobjectstore-clear-exception-order.js",
-    "idbobjectstore-delete-exception-order.js",
-    "idbobjectstore-deleteIndex-exception-order.js",
-    "idbobjectstore-query-exception-order.js",
 
     // No iframe in Node.js.
     "idbfactory-deleteDatabase-opaque-origin.js",
     "idbfactory-open-opaque-origin.js",
-
-    // Hangs because `dbname` is the same for all the async tests. If `dbname` was different for each async test, it
-    // would work.
-    "idbfactory_open9.js",
 
     // Fails because FDBTransaction._abort synchronously sends out error events to open requests, when it should be
     // asynchronous according to the spec. Making it asynchronous causes other tests to fail though. Need to be more
@@ -90,9 +81,6 @@ const skip = [
     // All kinds of fucked up.
     "open-request-queue.js",
 
-    // Usually works, but sometimes fails. Not sure why.
-    "parallel-cursors-upgrade.js",
-
     // Did not investigate in great detail.
     "bindings-inject-keys-bypass-setters.js",
     "bindings-inject-values-bypass-setters.js",
@@ -102,23 +90,17 @@ const skip = [
     "transaction-lifetime-empty.js",
     "upgrade-transaction-lifecycle-backend-aborted.js",
     "upgrade-transaction-lifecycle-user-aborted.js",
-
-    // Fails because `onerror` is never called since it is set after the abort call and the events on the request are
-    // triggered synchronously. Not sure how to reconcile this with the spec. Same issue affected some other test too, I
-    // think.
-    "transaction-abort-request-error.js",
 ];
 
 const filenames = glob.sync("/**/*.js", { root: testFolder });
 for (const absFilename of filenames) {
     const filename = path.relative(testFolder, absFilename);
     if (skip.includes(filename)) {
-        console.log(`Skipping ${filename}...\n`);
+        console.log(`Skipping ${filename}...`);
         skipped += 1;
         continue;
     }
 
-    console.log(`Running ${filename}...`);
     try {
         const output = execSync(`node ${filename}`, {
             cwd: testFolder,
@@ -126,11 +108,10 @@ for (const absFilename of filenames) {
         if (output.toString().length > 0) {
             console.log(output.toString());
         }
-        console.log("Success!\n");
         passed += 1;
     } catch (err) {
-        console.log("");
-        failed += 1;
+        console.log(`Failed at ${filename}`);
+        failed.push(filename);
     }
 }
 
@@ -140,12 +121,13 @@ if (skipped !== skip.length) {
 }
 
 console.log(`Passed: ${passed}`);
-console.log(`Failed: ${failed}`);
+console.log(`Failed: ${failed.length}`);
 console.log(`Skipped: ${skipped}\n`);
 
-const pct = Math.round((100 * passed) / (passed + failed + skipped));
+const pct = Math.round((100 * passed) / (passed + failed.length + skipped));
 console.log(`Success Rate: ${pct}%`);
 
-if (failed > 0) {
+if (failed.length > 0) {
+    console.log("Failed Tests: ", failed);
     process.exit(1);
 }
